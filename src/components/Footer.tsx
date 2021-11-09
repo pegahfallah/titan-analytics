d6d4d5fbbf0cfadd2056b00682b7963fe8b5e2b4
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-11-08 21:21:53 -0500

    modal

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,44 @@
+import Modal from "../components/Modal";
+import React from "react";
+
 function Footer() {
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
-    <div
-      id="footer"
-      className="px-sm flex flex-row m-auto items-center bg-blue bg-opacity-30 text-white border-opacity-40 "
-    >
-      <div className="pr-xs">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          width="24px"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
-          />
-        </svg>
+    <>
+      <Modal onClose={() => setShowModal(false)} show={showModal}></Modal>
+      <div
+        id="footer"
+        className="px-sm flex flex-row m-auto items-center bg-blue bg-opacity-30 text-white border-opacity-40 "
+      >
+        <div className="pr-xs">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            width="24px"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
+            />
+          </svg>
+        </div>
+        <h2 className="font-bold pr-xs">Did you miss early access?</h2>
+        <h2 className="pr-xs">
+          {" "}
+          Join the waitlist and you could still be one of the first to use
+          Titan.{" "}
+        </h2>
+        <button className="ml-auto transition duration-500 ease-in-out bg-blue bg-opacity-30 px-sm py-xs inline-block text-md leading-none rounded focus:shadow-outline hover:bg-opacity-70 text-white mt-4 lg:mt-0">
+          Sign up today
+        </button>
       </div>
-      <h2 className="font-bold pr-xs">Did you miss early access?</h2>
-      <h2 className="pr-xs">
-        {" "}
-        Join the waitlist and you could still be one of the first to use Titan.{" "}
-      </h2>
-      <button className="ml-auto transition duration-500 ease-in-out bg-blue bg-opacity-30 px-sm py-xs inline-block text-md leading-none rounded focus:shadow-outline hover:bg-opacity-70 text-white mt-4 lg:mt-0">
-        Sign up today
-      </button>
-    </div>
+    </>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,32 +1,40 @@
-function NavBar() {
-  return (
-    <nav className="flex items-center justify-between pt-xs">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <svg
-          width="26"
-          height="31"
-          viewBox="0 0 26 31"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M17.3502 11.0845L9.08106 7.90833C8.93573 7.85251 8.78394 7.97345 8.80587 8.12758L11.9163 29.9871C11.9306 30.0877 12.0167 30.1624 12.1183 30.1624H13.1922C13.2881 30.1624 13.3711 30.0956 13.3916 30.002L17.4764 11.3186C17.4979 11.2201 17.4443 11.1207 17.3502 11.0845Z"
-            fill="#EE86D0"
-          />
-          <path
-            d="M25.0015 0H0.296676C0.153591 0 0.0549359 0.14343 0.106114 0.27705L2.55846 6.6798C2.5887 6.75875 2.66448 6.81088 2.74902 6.81088H22.5634C22.648 6.81088 22.7239 6.75857 22.7541 6.67943L25.1922 0.27668C25.243 0.143119 25.1444 0 25.0015 0Z"
-            fill="#EE86D0"
-          />
-        </svg>
+import React from "react";
+import Modal from "../components/Modal";
 
-        <span className="font-semibold text-xl tracking-tight ml-xs">
-          TITAN
-        </span>
-      </div>
-      <div className="transition duration-500 ease-in-out bg-blue bg-opacity-30 px-sm py-xs inline-block text-md leading-none rounded focus:shadow-outline hover:bg-opacity-50 text-white mt-4 lg:mt-0">
-        <a href="#">Join the waitlist</a>
-      </div>
-    </nav>
+function NavBar() {
+  const [showModal, setShowModal] = React.useState(false);
+
+  return (
+    <>
+      <nav className="flex items-center justify-between pt-xs">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            width="26"
+            height="31"
+            viewBox="0 0 26 31"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.3502 11.0845L9.08106 7.90833C8.93573 7.85251 8.78394 7.97345 8.80587 8.12758L11.9163 29.9871C11.9306 30.0877 12.0167 30.1624 12.1183 30.1624H13.1922C13.2881 30.1624 13.3711 30.0956 13.3916 30.002L17.4764 11.3186C17.4979 11.2201 17.4443 11.1207 17.3502 11.0845Z"
+              fill="#EE86D0"
+            />
+            <path
+              d="M25.0015 0H0.296676C0.153591 0 0.0549359 0.14343 0.106114 0.27705L2.55846 6.6798C2.5887 6.75875 2.66448 6.81088 2.74902 6.81088H22.5634C22.648 6.81088 22.7239 6.75857 22.7541 6.67943L25.1922 0.27668C25.243 0.143119 25.1444 0 25.0015 0Z"
+              fill="#EE86D0"
+            />
+          </svg>
+
+          <span className="font-semibold text-xl tracking-tight ml-xs">
+            TITAN
+          </span>
+        </div>
+        <div className="transition duration-500 ease-in-out bg-blue bg-opacity-30 px-sm py-xs inline-block text-md leading-none rounded focus:shadow-outline hover:bg-opacity-50 text-white mt-4 lg:mt-0">
+          <button onClick={() => setShowModal(true)}>Join the waitlist</button>
+        </div>
+      </nav>
+      <Modal onClose={() => setShowModal(false)} show={showModal}></Modal>
+    </>
   );
 }
 
